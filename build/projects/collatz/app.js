e2f3dccfb01c5a51cd7193c439f23ef08b7913b0
Majid Laali <mjlaali@gmail.com>
2021-11-06 13:09:40 -0400

    Add interval check feature

--- build/projects/collatz/app.js
+++ build/projects/collatz/app.js
@@ -40,9 +40,10 @@ function collatz(a, b, c, n){
     }
 
     let repeat;
-    if (seen.has(n))
+    if (seen.has(n)){
         repeat = true;
-    else
+        seq.push(n);
+    } else
         repeat = false;
 
     return [seq, repeat];
@@ -57,7 +58,14 @@ function analyze(e) {
     const c = parseInt(mod.value)
 
     let [seq, repeated] = collatz(a, b, c, n);
-    sequence.nodeValue = JSON.stringify(seq).replaceAll(",", ", ");
+    
+    if (seq.length > 20){
+        let seq_start = JSON.stringify(seq.slice(0, 10)).replaceAll(",", ", ").slice(0, -1);
+        let seq_end = JSON.stringify(seq.slice(-10)).replaceAll(",", ", ").slice(1);
+        sequence.nodeValue = `${seq_start} ... ${seq_end}`;
+    } else {
+        sequence.nodeValue = JSON.stringify(seq).replaceAll(",", ", ");
+    }
     if (repeated){
         repeated_seq.classList.remove("hide");
         if (!no_repeated_seq.classList.contains("hide"))
@@ -68,4 +76,44 @@ function analyze(e) {
         no_repeated_seq.classList.remove("hide");
     }
 
+}
+
+
+interval = document.querySelector("#interval");
+
+interval.addEventListener("submit", compute_interval);
+
+function compute_interval(e){
+    e.preventDefault();
+
+    const a = parseInt(coeff.value);
+    const b = parseInt(bias.value);
+    const c = parseInt(mod.value)
+
+    const start = parseInt(document.querySelector("#start-interval").value);
+    const end = parseInt(document.querySelector("#end-interval").value);
+
+    let result_html = [];
+
+    for(let i = start; i < end; i++){
+        let [seq, repeated] = collatz(a, b, c, i);
+        let flag, color;
+        if (repeated){
+            flag = "&#10003;";
+            color = "green";
+        } else {
+            flag = "&#x274C;";
+            color = "red";
+        }
+
+        result_html.push(`
+        <tr class="${color}">
+            <td>${i}</td>
+            <td>${flag}</td>
+            <td>${Math.max( ...seq)}</td>
+        </tr>`);
+    }
+
+    periodic_numbers = document.querySelector("#periodic-numbers");
+    periodic_numbers.innerHTML = result_html.join(" ");
 }
